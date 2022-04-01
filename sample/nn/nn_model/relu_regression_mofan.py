99edfaf0dd3fdb0c4030d80b5f2399f6c860225a
pi667i <hua.fu@softlinker.com>
2022-04-01 17:23:15 +0800

    unsqueeze

--- sample/nn/nn_model/relu_regression_mofan.py
+++ sample/nn/nn_model/relu_regression_mofan.py
@@ -11,8 +11,9 @@ import matplotlib.pyplot as plt
 
 # torch.manual_seed(1)    # reproducible
 
-x = torch.unsqueeze(torch.linspace(-1, 1, 100), dim=1)  # x data (tensor), shape=(1,100)
+x = torch.unsqueeze(torch.linspace(-1, 1, 100), dim=0)  # x data (tensor), shape=(1,100)
 y = x.pow(2) + 0.2*torch.rand(x.size())                 # noisy y data (tensor), shape=(100, 1)
+
 
 # torch can only train on Variable, so convert them to Variable
 # The code below is deprecated in Pytorch 0.4. Now, autograd directly supports tensors
